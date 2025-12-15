9c73f07e4611ac37d3a4e16afab41b7946c81c38
narithsrun <112515078+narithsrun@users.noreply.github.com>
2025-12-15 00:25:07 -0500

    Time: 633 ms (12.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT employee_id, department_id 
+FROM 
+  (
+    SELECT 
+      *, 
+      COUNT(employee_id) OVER(PARTITION BY employee_id) AS EmployeeCount
+    FROM 
+      Employee
+  ) EmployeePartition 
+WHERE 
+  EmployeeCount = 1 
+  OR primary_flag = 'Y'
